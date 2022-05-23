f9dacb8f99153800dcee5e809c8290860c6aa56f
Dmitriy <milchenko-dima@rambler.ru>
2022-05-23 19:42:10 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+copyDir ();
+async function copyDir () {
+  await fsPromises.rm(path.join('04-copy-directory', 'files-copy'), {recursive : true, force : true});
+  await fsPromises.mkdir(path.join('04-copy-directory', 'files-copy'));
+
+
+  let files = await fsPromises.readdir(path.join('04-copy-directory', 'files'), { withFileTypes: true });
+  for (let file of files) {
+    if (file.isFile() === true) {
+      fsPromises.copyFile(path.join('04-copy-directory', 'files', file.name), 
+        path.join('04-copy-directory', 'files-copy', file.name));
+    }
+  }
+}
